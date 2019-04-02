bc2f8808d02a9c2a783ffaa24e6828c57c70f2ae
Ahmed Hadjou <thehidjou@yahoo.com>
2019-04-02 15:01:38 +0100

    CORS Middelware

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,9 @@ const functions = require('firebase-functions');
 const app = require('express')();
 const FBAuth = require('./util/fbAuth');
 
+const cors = require('cors');
+app.use(cors());
+
 const { db } = require('./util/admin');
 
 const {
