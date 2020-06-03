0c8bd1b0f72ffaeeaef66384a90508b0ba5f33c3
DileepAmrani <47317478+DileepAmrani@users.noreply.github.com>
2020-06-03 22:40:05 +0500

    config update
    Please accept pull request

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  apiKey: 'AIzaSyBGE61bBXSyRfOccvujvR8VjWfB1BjW-Og',
-  authDomain: 'socialape-d081e.firebaseapp.com',
-  databaseURL: 'https://socialape-d081e.firebaseio.com',
-  projectId: 'socialape-d081e',
-  storageBucket: 'socialape-d081e.appspot.com',
-  messagingSenderId: '354040522108'
+  apiKey: "AIzaSyDHxTu-ASLDU8Ji4NuocSc4uNdXAIzJcaI",
+  authDomain: "socialape-3a022.firebaseapp.com",
+  databaseURL: "https://socialape-3a022.firebaseio.com",
+  projectId: "socialape-3a022",
+  storageBucket: "socialape-3a022.appspot.com",
+  messagingSenderId: "455509939035",
+  appId: "1:455509939035:web:f162ae94733541c12fc07e",
+  measurementId: "G-76N8LNQS87"
 };
